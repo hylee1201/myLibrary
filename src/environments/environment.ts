7a1964e2c92d40147fe0bcfa0afcfb1ead52b72e
Haeyoung Lee <hylee1201@yahoo.com>
2020-02-09 09:44:47 -0500

    global stylesheet, assets files

---
+++ src/environments/environment.ts
@@ -0,0 +1,52 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDMMqg_INwpPtp2GogCJnbMZeuCfvrFyOE',
+    authDomain: 'hamayeon.firebaseapp.com',
+    databaseURL: 'https://hamayeon.firebaseio.com',
+    projectId: 'hamayeon',
+    storageBucket: 'hamayeon.appspot.com',
+    messagingSenderId: '435333837574',
+    appId: '1:435333837574:web:b1d4c9d9fc1994a562a487',
+    measurementId: 'G-DBCVCQY8H4'
+  },
+  firebaseAppEngineDefaultServiceAccount: {
+    type: 'service_account',
+    project_id: 'hamayeon',
+    private_key_id: 'e05127c1b7541277f6178de6c7a26cd5008e0b5b',
+    // tslint:disable-next-line:max-line-length
+    private_key: 'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC3z28GV74rMvDH\nA4EvGr3dmRDiogwVPIuJrl5Ku/bIhiMjCyY9VQmiTX2xYQ0S2+V3uvaOcgiCGVAm\nq1pkbh5JOB1QbVUjYD5zjF4CsLWgY3bYG+tK0TkvodubXe4JwdNm2sqxtsagCgPH\nr8UXiejmNPiB5eywH+y6GZ5IypuF55dE6UdtzZ9IG1k2imaaI/HKcBWry2009Q3L\n4vEOIRB66yX+O6Yyp+iET25CfDA1UVDFrGLRjAwkv2tYFrhYTCk9MBJPRWcuDb5b\nHCiwOZdtX4DI0ORA1/DtjVFkDg5puo04G8bgkHkSfmOYwtcf2v6Hhtn4wCnYjSmf\njJoQIhGLAgMBAAECggEARjsHSEyhOB9EGT5RDz7v+aoA5h8Vb83M4QPTOkncTpzV\n8w9RApEp1641Dt+un5yObjNs/ls6vf7O/Z9uL75VaxN6O98ybwxYgEzLJMsA8rHe\nWE8+lT0g1P7KinAWwfqhfASv+rwD42VAMX2K2GYUq1Tk+LjxuQ/Rov+j11s2ljAT\nYqKuQjAITRhP0Sp7cpgONQVUv7I6zWkB/Jd0pDsIH4iqZcAZZE/WsldTNAPhdSKv\n0qgIb0QqKyYvIRI29u805u2sq55XNPEMF9clyPu53flaFkvoLpfla6ecwNVUOud/\n/DZDJJ55MMGQOyRcFmm4J5tIcalKzI/ihOvzOMeEDQKBgQDgde/AAzg2OqR9J5Mw\n9GSUJgMN2YaOuIS66Kk3F+em4xMAZ/gtr9hKuWZrm68ZxZrDXHo3bZ0x7Gt6q9vQ\n/Un+H3Qwd5+DmGOPTsshGZy8PUMXUjY/4idDFv+Dwc7rEt4/jsCMkVSpiQk7Bs70\nl5pFrwhH8qU2v03EDaqG16aK/wKBgQDRo0OEXwCrb+uCitHOUtChTipN9X/MCAbD\nQeuxklL85ujRHI8FP2u9CJpyqUOdBH5JwkjULR/IT3r5uRfABPDSJ9A9vOSluY6C\nMkybUh7lDn58Gq4aRD3HFueu/VTeCEAMqfOvr554BFCyXAnXrWGW94x6l6Dd6Cxa\n0mdOMoJ1dQKBgD7KHwgqnmXlH7B3CqDOLPiYbZBJQm/YRCD5BqUHxeCUcvNRIka9\ngxbcKD9/7IqcEYAG9ZVdz3Rwu1w0iSg8Qp+Y8uq/+9pXHbqwAvi+bm7mAEec0toq\nDGujGlUkORrfS1aiO1DXMsS+NdvSOsCHTDukC//8a6UV/M4UOJjB6K/HAoGAT7nj\nh4gJ3SknS/I3cFwWdAIiHJvtCk9TIxcC5/laDkxS5pQR4FEKTE3lTw4IZMK8tJhq\n0iaHG8r+XdKCscgvPf1eONyJqG01QVaXw8orbblhaRlMdTjWtcyEOAyoJfjFQwuv\nXXVQsxwRBIP0Sk3p+1wR1N2tlDckBzZVi2uyKbECgYBf428s3VYByO4AjICWNw6Q\n8QgO8a3ZXFi6Ynw1SqnTR3RPoB+hASMR610bWmp6cr0j/w5Zd4NGs2qdg/TgUKah\ntSnN4hyGdGz8dFZY1eeJYNgEd7XhR8gGh2XpHjwgMjNzkKB1hbxwL7lBAI/Km9RQ\n+ReW7HPhCoXI3vmoZXz3Dw==',
+    client_email: 'hamayeon@appspot.gserviceaccount.com',
+    client_id: '101179412629246937633',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/hamayeon%40appspot.gserviceaccount.com'
+  },
+  firebaseSdkAdminServiceAccount: {
+    type: 'service_account',
+    project_id: 'hamayeon',
+    private_key_id: '9e20078cf663198b984c7b6a289788dfafdafaf4',
+    // tslint:disable-next-line:max-line-length
+    private_key: 'MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC7V31EFqrmNFXg\ngu5IpdfAkoKpEFdpR3fyrBTjt/LluRk19cpMSfAXv31676uPGp/j27WhpPfkBrza\nK9qvbFZgvyedYSzwhYitzLag86Dm/nfT642GQVu/jQeKt7U2bcho0//BJHO/RpXK\nOEvJ2ZH766+QqFCHaA6uLjnr3NOb3QFn0WjYa46MDfbsPthPL18IVXt+1A2uar6n\nmYp/V3NMj41bScIKFV/o45FMs0UR9gJ3cdFtUXkb9h9S1cD4iDJmzqpCIB3Szn6E\nuzSnkEOzDPGYZVySrUcBixCSuHNiFwSexPI3p1MUrt8yiYLE12YCZ/C5GZQ31ah9\ntJwJzkUpAgMBAAECggEAHl9LxrsqD9JSTejMOujwNp9KIaCtmzrj/mrZpCWRcp70\nsGPd3Upra143OLQci0HUxDs9kixNqCFTgsdzV9dnlZ3IDWLQx1nO0+aJgUSeRHTi\n9m+M7UQxXLe/jkTEU0OvJxefsi93uH34EOXZm6rzWPVX7Erta1Z6ctC5wTx27iC6\nVwZQpEaEekf/z4ISeQRbKp5TujiQRzzBxvPzxUFg34fcXoEMZ8NIEAERi+UAoC68\ne4bQakUp9fslHt/51sFv/GPK0AKsA90g3m8N7dEsQYIWwZB82Nw+/OeSvEmjYyfj\nuOuRWs7Ap/D+V5vB+jKH1MkWnck1DYtxiI5SH+yM1QKBgQDnwa80vTVfNFlBdCVl\nSqcZDMXhY1ILu8W+NSW5ZXCiIPss6oJQ4Z5YbZMER+9posuzXK3jPMMsXk3dNBwA\nC1quR3iyxwktAv1aEyYngPKABfpqNKXI0wVpdjoL2LnfL+vnA/2LYQB8EMclI/jg\nAK74MzCMvUfSRpQRuG5MSHQRrwKBgQDO8GZl3togk+fVmTUiRrLqYJ+2bc5sECiz\nKP73jOY2LMpsF+LEVePRgQWUrnJbuyATuvXCKu2P9b7tq27T60qzR1yKH1PZmKNS\nYKuFvrhQdza2PKlsu11eu7VCN9Q6sPgWVTAoBB1fLbFdFlhQUTd8sSYa+0T6wyb2\ni3PI6zAEpwKBgB9eyT+DSQEtDdPdS96NH1c1smdS0V1DOn4HpaaVw4w+mKE40RDv\nqUZg9qCCuDZ9UcNijSsQI38S+Mkzb4LWBp1GkWDpriVInVCe/PAl9S09IetUWwh2\n/+mJlkvaNzatvunTUXOXWsBwaPRvDRYNIaH5E95JFLqy0QIhC9UJjaSDAoGAWDKl\nse1VUmU3fhYaS02Om84qw/nw7KXGcvWaFa/7vkNA0ukrQTrKKlSvKDthii8q8L3S\nWLsrHRxED8/tIG08HUTg8A2CE0+27Dk/YXVz4PeuXeVk4G7K0MF3840P6yPZ2Mre\nB3AZhKNY4K/RXAoBeCeYcg99ytK6HII7CvsSMCkCgYEAoCaLmGui+SNXF3uIuB0z\nN/eOEmhys6EH0gt8fzsiA3w/nxRFuZmxEtp/kZkUCvYDTH2Cpdpbeq/vY/COeONI\nbL2NguwffrzdH3MuyyewxY1cMEx1QzVYpR1JTFws0juaR+YizSQZEixZZTjp0xJj\nh+JMDpMKA8Nl1hf/G5x5IQ0=',
+    client_email: 'firebase-adminsdk-d9nt5@hamayeon.iam.gserviceaccount.com',
+    client_id: '102996240376117597603',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-d9nt5%40hamayeon.iam.gserviceaccount.com'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
